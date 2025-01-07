00db954e4561ff9b6264a3b99fbc522302d459f1
Michael <optexyz@gmail.com>
2025-01-07 18:31:40 -0500

    initial commit

---
+++ src/main/java/com/codedifferently/CD_InternTracker/CdInternTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.codedifferently.CD_InternTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdInternTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdInternTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codedifferently/CD_InternTracker/CdInternTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedifferently.CD_InternTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdInternTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
