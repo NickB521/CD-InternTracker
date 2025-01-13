bde0d03d8bfa99b0207241f85fe386c01f42a6e3
Michael <optexyz@gmail.com>
2025-01-13 17:32:25 -0500

    changed api routes to use singular language

--- src/main/java/com/codedifferently/CD_InternTracker/controllers/InternController.java
+++ src/main/java/com/codedifferently/CD_InternTracker/controllers/InternController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/interns")
+@RequestMapping("/api/intern")
 public class InternController {
     private InternService internService;
 
