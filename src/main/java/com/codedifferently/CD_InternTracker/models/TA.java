2caa1ce640e0981cc0b1caf99056b1476ab16a59
Naseru <nasirdover05@gmail.com>
2025-02-07 17:55:26 -0500

    Fixed Errors in TA.java

--- src/main/java/com/codedifferently/CD_InternTracker/models/TA.java
+++ src/main/java/com/codedifferently/CD_InternTracker/models/TA.java
@@ -1,71 +1,31 @@
 package com.codedifferently.CD_InternTracker.models;
 
 import jakarta.persistence.*;
-import org.springframework.lang.NonNull;
 import lombok.*;
 
-import java.util.List;
-
 @Entity
-@NoArgsConstructor
-@RequiredArgsConstructor
-@Data
-
-
-
 @Table(name = "app_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TA {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String phoneNumber;
 
-    // Getter and Setter for id
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    // Getter and Setter for name
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    // Getter and Setter for email
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    // Getter and Setter for password
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    // Getter and Setter for phone number
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+    private boolean isAdmin;
+    private boolean isTA;
 }
-
-
