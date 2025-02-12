d9c84bc05e23d0667e3a2d31c13ddf146bdc5066
mekhiStarcks <122574138+mekhiStarcks@users.noreply.github.com>
2025-02-12 18:26:18 -0500

    Update Intern.java

--- src/main/java/com/codedifferently/CD_InternTracker/models/Intern.java
+++ src/main/java/com/codedifferently/CD_InternTracker/models/Intern.java
@@ -7,85 +7,40 @@ import java.util.List;
 
 @Entity
 public class Intern {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private String name;
+    private String email;
     private Long id;
 
-    @NonNull
-    private String name;
+    // Constructor to initialize Intern with name, email, and id
+    public Intern(String name, String email, Long id) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+    }
 
-    @NonNull
-    private String email;
-    private String level;
-
-
-    @NonNull
-    private String phoneNumber;
-
-    private String internNotes;
-    @ElementCollection
-    private List<DailySchedule> weeklySchedule;
-    @ElementCollection
-    private List<Day> attendance;
-    @NonNull
-    public Long getId() {return id;}
-
-    @NonNull
+    // Getters and setters (if needed)
     public String getName() {
         return name;
     }
 
-    public void setName(@NonNull String name) {
+    public void setName(String name) {
         this.name = name;
     }
 
-
-    @NonNull
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(@NonNull String email) {
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    @NonNull
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public Long getId() {
+        return id;
     }
 
-    public void setPhoneNumber(@NonNull String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getInternNotes() {
-        return internNotes;
-    }
-
-    public void setInternNotes(String internNotes) {
-        this.internNotes = internNotes;
-    }
-    public List<DailySchedule> getWeeklySchedule() {
-        return weeklySchedule;
-    }
-
-    public void setWeeklySchedule(List<DailySchedule> weeklySchedule) {
-        this.weeklySchedule = weeklySchedule;
-    }
-
-    public List<Day> getAttendance() {
-        return attendance;
-    }
-
-    public void setAttendance(List<Day> attendance) {
-        this.attendance = attendance;
-    }
-
-    public String getLevel() {
-        return level;
-    }
-
-    public void setLevel(String level) {
-        this.level = level;
+    public void setId(Long id) {
+        this.id = id;
     }
 }
