bd30890c2c66f9d33975e805759c55a6df85b5c0
Naseru <nasirdover05@gmail.com>
2025-02-12 17:14:40 -0500

    Added imports

--- src/main/java/com/codedifferently/CD_InternTracker/storage/JsonDataStorage.java
+++ src/main/java/com/codedifferently/CD_InternTracker/storage/JsonDataStorage.java
@@ -1,5 +1,22 @@
 package com.codedifferently.CD_InternTracker.storage;
 
+import com.codedifferently.CD_InternTracker.models.Intern;
+import com.codedifferently.CD_InternTracker.models.TA;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class JsonDataStorage {
-    
+    private static final String FILE_PATH = "storage/JsonDataStorage.json"; 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        ensureStorageExists();
+    }
+
 }
