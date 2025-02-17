906889338170ce19c729f1f90bbf2f91aff17628
mekhiStarcks <122574138+mekhiStarcks@users.noreply.github.com>
2025-02-17 18:47:21 -0500

    Update InternServiceImpl.java

--- src/main/java/com/codedifferently/CD_InternTracker/services/InternServiceImpl.java
+++ src/main/java/com/codedifferently/CD_InternTracker/services/InternServiceImpl.java
@@ -6,7 +6,7 @@ import com.codedifferently.CD_InternTracker.models.DailySchedule;
 import com.codedifferently.CD_InternTracker.models.Intern;
 import com.codedifferently.CD_InternTracker.utils.ValidationUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Pair;
+import org.antlr.v4.runtime.misc.Pair;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
