dbd4e8a9139b444344d808976512f58ff5477895
Naseru <nasirdover05@gmail.com>
2025-02-20 18:13:19 -0500

    Trying to fix stuff

--- src/main/java/com/codedifferently/CD_InternTracker/models/DailySchedule.java
+++ src/main/java/com/codedifferently/CD_InternTracker/models/DailySchedule.java
@@ -1,7 +1,10 @@
 package com.codedifferently.CD_InternTracker.models;
 
-
-import jakarta.persistence.Embeddable;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.lang.NonNull;
 
 @Entity
@@ -25,45 +28,6 @@ public class DailySchedule {
     @NonNull
     private String endTime;
 
-    // Default constructor
-    public DailySchedule() {
-    }
-
-
-    // Constructor with parameters
-    public DailySchedule(String weekDay, String startTime, String endTime) {
-        this.weekDay = weekDay;
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
-
-    // Getters and setters
-
-    public String getWeekDay() {
-        return weekDay;
-    }
-
-    public void setWeekDay(String weekDay) {
-        this.weekDay = weekDay;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-
-    // Override toString method for better representation
     @Override
     public String toString() {
         return "DailySchedule{" +
