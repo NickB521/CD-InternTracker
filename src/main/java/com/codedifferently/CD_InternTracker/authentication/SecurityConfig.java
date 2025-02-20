5cdbcd4cb72e7271dcd262458cae1f00ba44c2ae
mekhiStarcks <122574138+mekhiStarcks@users.noreply.github.com>
2025-02-20 17:32:50 -0500

    Update SecurityConfig.java

--- src/main/java/com/codedifferently/CD_InternTracker/authentication/SecurityConfig.java
+++ src/main/java/com/codedifferently/CD_InternTracker/authentication/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @PostConstruct
     public void seedAdminUser() {
         // Seeding an admin user with role "admin"
-        TA adminUser = new TA("admin123", "admin@example.com", "0123456789", "Michael Womer", "admin", true, true);
+        TA adminUser = new TA("admin123", "admin@example.com", "0123456789", "Michael Womer", true, true);
         TAService.create(adminUser);
         System.out.println("Test admin seeded");
         System.out.println("email: admin@example.com");
@@ -70,7 +70,7 @@ public class SecurityConfig {
     @PostConstruct
     public void seedUser() {
         // Seeding a regular user with role "user"
-        TA user = new TA("user123", "user@example.com", "0123456890", "Michael Womer", "user", false, true);
+        TA user = new TA("user123", "user@example.com", "0123456890", "Michael Womer", false, true);
         TAService.create(user);
         System.out.println("Test user seeded");
         System.out.println("email: user@example.com");
